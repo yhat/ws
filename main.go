ea19b96c4bbb6404458c170cfc1923d426160f79
Eric Chiang <eric.chiang.m@gmail.com>
2015-01-08 12:38:35 -0500

    more options added

--- main.go
+++ main.go
@@ -4,28 +4,69 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"net/http"
 	"os"
+	"strings"
 
 	"golang.org/x/net/websocket"
 )
 
-var origin string
+var (
+	origin  string
+	headers string
+	version int
+)
 
 func init() {
-	flag.StringVar(&origin, "origin", "http://localhost/", "websocket origin")
+	help = fmt.Sprintf(help, VERSION)
+	flag.StringVar(&origin, "o", "http://0.0.0.0/", "websocket origin")
+	flag.StringVar(&headers, "H", "", "a comma separated list of http headers")
+	flag.IntVar(&version, "v", websocket.ProtocolVersionHybi13, "websocket version")
 	flag.Parse()
 }
 
+const VERSION = "0.1"
+
+var help = `ws - %s
+
+Usage:
+	ws [options] <url>
+
+Use "ws --help" for help.
+`
+
+func parseHeaders(headers string) http.Header {
+	h := http.Header{}
+	for _, header := range strings.Split(headers, ",") {
+		parts := strings.SplitN(header, ":", 2)
+		if len(parts) != 2 {
+			continue
+		}
+		h.Add(strings.TrimSpace(parts[0]), strings.TrimSpace(parts[1]))
+	}
+	return h
+}
+
 func main() {
-	url := flag.Arg(0)
-	if url == "" {
-		fmt.Fprintf(os.Stderr, "Usage: ws <url>\n")
+	fatal := func(format string, a ...interface{}) {
+		fmt.Fprintf(os.Stderr, format, a...)
 		os.Exit(2)
 	}
-	ws, err := websocket.Dial(url, "", origin)
+	target := flag.Arg(0)
+	if target == "" {
+		fatal(help)
+	}
+	config, err := websocket.NewConfig(target, origin)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error dialing %s: %v\n", url, err)
-		os.Exit(2)
+		fatal("%s\n", err)
+	}
+	if headers != "" {
+		config.Header = parseHeaders(headers)
+	}
+	config.Version = version
+	ws, err := websocket.DialConfig(config)
+	if err != nil {
+		fatal("Error dialing %s: %v\n", target, err)
 	}
 	errc := make(chan error, 2)
 	cp := func(dst io.Writer, src io.Reader) {
